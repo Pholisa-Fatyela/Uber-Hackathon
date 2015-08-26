241194336c6b956e36f34899c80d3f67e9b5c910
Pholisa-Fatyela <pholisa@projectcodex.co>
2015-08-26 17:32:59 +0200

    Modified problems handlebars page

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 var express = require('express'),
-<<<<<<< HEAD
-exphbs  = require('express3-handlebars'),
-
- 
-=======
     exphbs  = require('express3-handlebars'),
-
->>>>>>> 29152ac63a971288dc3e71e1f5ee87228c0a3889
     app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -16,13 +9,11 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-<<<<<<< HEAD
 app.get('/appointment', function (req, res){
 	res.render('appointment', {layout: false});
 });
- 
-app.listen(3000);
-=======
+
+
 var port = process.env.PORT || 5000;
 
 var server = app.listen(port, function () {
@@ -33,4 +24,3 @@ var server = app.listen(port, function () {
   console.log('Uber app listening at http://%s:%s', host, port);
 
 });
->>>>>>> 29152ac63a971288dc3e71e1f5ee87228c0a3889
