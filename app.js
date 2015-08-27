46bbc85b91011d2c58ad561b8f052fcdcc8ee4c9
Nwabisa <linkie@projectCodex.co>
2015-08-27 14:10:22 +0200

    home page and templates

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var express = require('express'),
-<<<<<<< HEAD
-    exphbs  = require('express3-handlebars'),
-=======
+
 exphbs  = require('express3-handlebars'),
 
->>>>>>> e0821808556b23f3b4c2ded9632427accafc5740
     app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -17,21 +14,6 @@ app.get('/', function (req, res) {
 app.get('/appointment', function (req, res){
 	res.render('appointment', {layout: false});
 });
-<<<<<<< HEAD
 
-
-var port = process.env.PORT || 5000;
-
-var server = app.listen(port, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Uber app listening at http://%s:%s', host, port);
-
-});
-=======
  
 app.listen(3000);
-
->>>>>>> e0821808556b23f3b4c2ded9632427accafc5740
