1a96d0a0300dd26811411ee54ee32419ea94e57d
Pretty <pretty@projectcodex.co>
2015-08-27 12:33:20 +0200

    changes committed

--- app.js
+++ app.js
@@ -1,37 +1,57 @@
+'use strict';
+
 var express = require('express'),
-<<<<<<< HEAD
-    exphbs  = require('express3-handlebars'),
-=======
-exphbs  = require('express3-handlebars'),
+    exphbs  = require('express-handlebars'),
+    mysql = require('mysql'), 
+    myConnection = require('express-myconnection'),
+    bodyParser = require('body-parser'),
+    partners = require('./routes/partners');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
->>>>>>> e0821808556b23f3b4c2ded9632427accafc5740
-    app = express();
+var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+var dbOptions = {
+      host: 'localhost',
+      user: 'root',
+      password: 'linokuhlekamva',
+      port: 3306,
+      database: 'UberHackathon'
+};
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(myConnection(mysql, dbOptions, 'single'));
+app.use(cookieParser('shhhh, very secret'));
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 3600000 }, resave: true, saveUninitialized: true})); -
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/appointment', function (req, res){
-	res.render('appointment', {layout: false});
+  res.render('appointment', {layout: false});
 });
-<<<<<<< HEAD
 
+app.get('/register', function (req, res){
+  res.render('register', {layout: false});
+})
 
-var port = process.env.PORT || 5000;
-
-var server = app.listen(port, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Uber app listening at http://%s:%s', host, port);
-
+app.get('/page', function (req, res){
+  res.render('page', {layout: false});
 });
-=======
- 
+
+//setup the handlers
+app.get('/partners', partners.show);
+app.get('/partners/edit/:id', partners.get);
+app.post('/partners/update/:id', partners.update);
+app.post('/partners/add', partners.add);
+//this should be a post but this is only an illustration of CRUD - not on good practices
+app.get('/partners/delete/:id', partners.delete);
+
 app.listen(3000);
-
->>>>>>> e0821808556b23f3b4c2ded9632427accafc5740
