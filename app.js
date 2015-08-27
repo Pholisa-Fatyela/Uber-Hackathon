63873c3092f91713e1c5e226a37b7cd9e3c85d78
Pholisa-Fatyela <pholisa@projectcodex.co>
2015-08-27 14:04:46 +0200

    linked register page

--- app.js
+++ app.js
@@ -1,55 +1,37 @@
 var express = require('express'),
-    exphbs  = require('express-handlebars'),
-    mysql = require('mysql'), 
-    myConnection = require('express-myconnection'),
-    bodyParser = require('body-parser'),
-    partners = require('./routes/partners');
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
+    exphbs  = require('express3-handlebars'),
+    app = express();
 
-var app = express();
-
-var dbOptions = {
-      host: 'localhost',
-      user: 'root',
-      password: 'linokuhlekamva',
-      port: 3306,
-      database: 'UberHackathon'
-};
-
-app.use(express.static(__dirname + '/public'));
-
-app.use(myConnection(mysql, dbOptions, 'single'));
-app.use(cookieParser('shhhh, very secret'));
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 3600000 }, resave: true, saveUninitialized: true})); -
-app.use(express.static('public'));
-app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/login', function (req, res){
+	res.render('login', {layout: false});
+});
+
 app.get('/appointment', function (req, res){
-  res.render('appointment', {layout: false});
+	res.render('appointment', {layout: false});
 });
 
 app.get('/register', function (req, res){
-  res.render('register', {layout: false});
-})
-
-app.get('/page', function (req, res){
-  res.render('page', {layout: false});
+	res.render('register', {layout: false});
 });
 
-//setup the handlers
-app.get('/partners', partners.show);
-app.get('/partners/edit/:id', partners.get);
-app.post('/partners/update/:id', partners.update);
-app.post('/partners/add', partners.add);
-//this should be a post but this is only an illustration of CRUD - not on good practices
-app.get('/partners/delete/:id', partners.delete);
+app.get('/page', function (req, res){
+	res.render('page', {layout: false});
+});
 
-app.listen(3000);
+var port = process.env.PORT || 5000;
+
+var server = app.listen(port, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Uber app listening at http://%s:%s', host, port);
+
+});
